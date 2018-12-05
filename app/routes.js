49a28ad881c85945022c8ec2a7345f9a64ed89c1
hardik <hardikpancholi85@gmail.com>
2018-12-05 15:31:19 +0530

    test without db

--- app/routes.js
+++ app/routes.js
@@ -2,16 +2,18 @@ var db = require('../connection');
 
 function getTodos(req, res) {
 
-    db.query("SELECT Id, AllowedAPICallInOneMinute, UserId FROM `gtl_api_key` \
-                WHERE `SecretKey` = ? and Active=1", [req.query.apikey], function (err, result, fields) {
-            if (result[0] == undefined) {
-                res.json("Your key is : " + req.query.apikey); // return all todos in JSON format};
-            }
-            else
-            {
-                res.json("Your key is : " + req.query.apikey); // return all todos in JSON format};
-            }
-        });
+    res.json("Your key is : " + req.query.apikey); // return all todos in JSON format};
+
+    // db.query("SELECT Id, AllowedAPICallInOneMinute, UserId FROM `gtl_api_key` \
+    //             WHERE `SecretKey` = ? and Active=1", [req.query.apikey], function (err, result, fields) {
+    //         if (result[0] == undefined) {
+    //             res.json("Your key is : " + req.query.apikey); // return all todos in JSON format};
+    //         }
+    //         else
+    //         {
+    //             res.json("Your key is : " + req.query.apikey); // return all todos in JSON format};
+    //         }
+    //     });
 
 
 }
