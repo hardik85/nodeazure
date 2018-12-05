03f305303dd94cd5e77807d6f80b8e46fba47cee
hardik <hardikpancholi85@gmail.com>
2018-12-05 14:19:13 +0530

    removed commented code

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-//app.use(express.static(__dirname + '/public'));
-
 app.get('/', function (req, res) {
     console.log('hello from server');
     //    res.render('./public/index.html');
